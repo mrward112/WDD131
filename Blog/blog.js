562fa94d2c0128c30a187410c684bcb7bc82ba51
mrward112 <merrickrward@gmail.com>
2025-02-17 15:12:39 -0700

    update to blog project

--- Blog/blog.js
+++ Blog/blog.js
@@ -1,27 +1,73 @@
 const articles = [
-	{
-		id: 1,
-		title: 'Septimus Heap Book One: Magyk',
-		date: 'July 5, 2022',
+    {
+        id: 1,
+        title: 'Septimus Heap Book One: Magyk',
+        date: 'July 5, 2022',
+        description: 'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
+        imgAlt: 'Book cover for Septimus Heap 1',
+        ages: '10-14',
+        genre: 'Fantasy',
+        stars: '⭐⭐⭐⭐',
+        author: 'Angie Sage'
+    },
+    {
+        id: 2,
+        title: 'Magnus Chase Book One: Sword of Summer',
+        date: 'December 12, 2021',
+        description: 'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+        imgSrc: 'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+        imgAlt: 'Book cover for Magnus Chase 1',
+        ages: '12-16',
+        genre: 'Fantasy',
+        stars: '⭐⭐⭐⭐',
+        author: 'Rick Riordan'
+    },
+    {
+        id: 3,
+        title: "Belgariad Book One: Pawn of Prophecy",
+        date: "Feb 12, 2022",
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
 		description:
-			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
-		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
-		imgAlt: 'Book cover for Septimus Heap 1',
-		ages: '10-14',
-		genre: 'Fantasy',
-		stars: '****'
-	},
-	{
-		id: 2,
-		title: 'Magnus Chase Book One: Sword of Summer',
-		date: 'December 12, 2021',
-		description:
-			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
 		imgSrc:
-			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
-		imgAlt: 'Book cover for Magnus Chase 1',
-		ages: '12-16',
-		genre: 'Fantasy',
-		stars: '⭐⭐⭐⭐'
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "*****",
+		author: "David Eddings"
 	}
 ]
+
+// Get a reference to the element we want to insert our articles into
+const articlesContainer = document.querySelector('#book-list ul');
+const bookInfoSection = document.querySelector('#book-info');
+
+// Function to build and output the HTML for the list of articles
+function displayArticles(articles) {
+    articles.forEach(article => {
+        // Create a new list item element
+        const articleElement = document.createElement('li');
+        articleElement.classList.add('article'); // Add any classes you need
+
+        // Create a template literal string for the article content
+        const articleContent = `
+            <h3>${article.title}</h3>
+            <img src="${article.imgSrc}" alt="${article.imgAlt}" class="book-cover">
+            <p>Author: ${article.author}</p>
+            <p>Summary: ${article.description} <a href="#">Read More...</a></p>
+        `;
+
+        // Set the innerHTML of the new article element to the template literal string
+        articleElement.innerHTML = articleContent;
+
+        // Append the new article element to the output element
+        articlesContainer.appendChild(articleElement);
+    });
+}
+
+// Call the function to display the articles
+displayArticles(articles);
